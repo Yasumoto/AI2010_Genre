50cf033b04bca2f5b3c43d82088b2dee0447612a
linzy <linzy@10.137.3.120>
2010-05-16 16:37:20 -0700

    blahhh

---
+++ src/com/bjoli/genre/Classifier.java
@@ -0,0 +1,16 @@
+package com.bjoli.genre;
+
+import java.io.File;
+
+public class Classifier
+{
+	public void train(File dir)
+	{
+		
+	}
+	
+	public String classify(File file)
+	{
+		return null;
+	}
+}
